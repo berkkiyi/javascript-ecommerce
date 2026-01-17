535bdc86407614b8e2633ed2cbe91500a333f82c
Berk <kyberk1234@gmail.com>
2026-01-18 00:16:40 +0300

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,12 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -27,7 +29,7 @@ export function addToCart(productId) {
   });
 
   const quantitySelector = document.querySelector(
-    `.js-quantity-selector-${productId}`
+    `.js-quantity-selector-${productId}`,
   );
 
   const quantity = Number(quantitySelector.value);
@@ -38,6 +40,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
 
